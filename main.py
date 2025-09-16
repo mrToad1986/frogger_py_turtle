f46aba91f557587df708aa68854e1fb0e79ee4e4
mrToad1986 <korostylev.ov@gmail.com>
2025-09-16 22:23:12 +0300

    update cars

--- main.py
+++ main.py
@@ -37,16 +37,16 @@ class Player(Sprite):
         Sprite.__init__(self, x, y, width, height, image)
 
     def up(self):
-        self.y += 45
+        self.y += 50
 
     def down(self):
-        self.y -= 45
+        self.y -= 50
 
     def right(self):
-        self.x += 45
+        self.x += 50
 
     def left(self):
-        self.x -= 45
+        self.x -= 50
 class Car(Sprite):
     def __init__(self, x, y, width, height, image, dx):
         Sprite.__init__(self, x, y, width, height, image)
@@ -54,11 +54,18 @@ class Car(Sprite):
     def update(self):
         self.x += self.dx
 
+        # Border cheking
+        if self.x < - 400:
+            self.x = 400
+        if self.x > 400:
+            self.x = -400
+
 # Create objects
 player = Player(0, -300, 40, 40, 'media/frog.gif')
 player.render(pen)
 
-car_left = Car(0, -255, 40, 121, 'media/car_left.gif', -0.05)
+car_left = Car(0, -250, 40, 121, 'media/car_left.gif', -0.08)
+car_right = Car(0, -200, 40, 121, 'media/car_right.gif', 0.08)
 
 # Keyboard binding
 wn.listen()
@@ -71,9 +78,11 @@ while True:
     # render
     player.render(pen)
     car_left.render(pen)
+    car_right.render(pen)
 
     #update objects
     car_left.update()
+    car_right.update()
 
     # update screen
     wn.update()
